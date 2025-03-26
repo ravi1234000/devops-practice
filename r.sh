4c0d923798e23755f33ed24ea7d5b99d968f921b
ravi <alluravikishore@gmail.com>
2025-03-26 17:11:02 +0530

    Merge remote-tracking branch 'origin/main'frontend hello

--- r.sh
+++ r.sh
@@ -5,4 +5,4 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-cp catalogue.service /etc/nginx/default.d/catalogue.service
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf
