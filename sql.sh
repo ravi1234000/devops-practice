9f6ebf1155623bc58106b1c2876b9be4435d5254
ravi <alluravikishore@gmail.com>
2025-03-28 15:35:39 +0530

    Merge remote-tracking branch 'origin/main'bashadengubasha

--- sql.sh
+++ sql.sh
@@ -1,6 +1,6 @@
 dnf module disable mysql -y
 cp mysql.repo /etc/yum.repos.d/mysql.repo
 dnf install mysql-community-server -y
-systemctl enable mysql
-systemctl restart mysql
+systemctl enable mysqld
+systemctl restart mysqld
 mysql_secure_installtion --set-root-pass Roboshop@1
