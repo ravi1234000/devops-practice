efb2cdac30ba46494cd8ebcd8042769440f6b349
ravi <alluravikishore@gmail.com>
2025-03-28 11:55:57 +0530

    Merge remote-tracking branch 'origin/main'bashadengubasha

--- mongod.sh
+++ mongod.sh
@@ -2,4 +2,4 @@ cp mongod.repo /etc/yum.repos.d/mongod.repo
 dnf install mongodb-org -y
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 systemctl enable mongod
-systemctl start mongod
+systemctl restart mongod
