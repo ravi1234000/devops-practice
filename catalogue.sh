e9a8633f702ee24104455c026fb46008f4fc5f18
ravi <alluravikishore@gmail.com>
2025-03-28 14:11:23 +0530

    Merge remote-tracking branch 'origin/main'bashadengubasha

--- catalogue.sh
+++ catalogue.sh
@@ -6,17 +6,18 @@ echo -e "\e[36m<<<<<<<munda>>>>>>> \e [0m"
 
 useradd roboshop
 mkdir /app
-echp -e "\e[36m<<<<<<<pukku>>>>>>> \e [0m"
+echo -e "\e[36m<<<<<<<pukku>>>>>>> \e [0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
-echp -e "\e[36m<<<<<<<pukku>>>>>>> \e [0m"
+echo -e "\e[36m<<<<<<<pukku>>>>>>> \e [0m"
 unzip /tmp/catalogue.zip
 npm install
-echp -e "\e[36m<<<<<<<naukku>>>>>>> \e [0m"
+echo -e "\e[36m<<<<<<<naukku>>>>>>> \e [0m"
 cp /home/centos/devops-practice/catalogue.service /etc/systemd/system/catalogue.service
-echp -e "\e[36m<<<<<<<boukku>>>>>>> \e [0m"
+echo -e "\e[36m<<<<<<<boukku>>>>>>> \e [0m"
+systemctl daemon-reload
 systemctl  enable catalogue
-systemctl start catalogue
+systemctl restart catalogue
 cp /home/centos/devops-practice/mongod.repo  /etc/yum.repos.d/mongod.repo
 yum install mongodb-org-shell -y
 mongo --host <ip address> </app/schema/catalogue.js
