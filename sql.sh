90b284f440503873cbf10af03ccbc6b07a89e6b4
ravi <alluravikishore@gmail.com>
2025-03-28 15:36:57 +0530

    Merge remote-tracking branch 'origin/main'bashadengubasha

--- sql.sh
+++ sql.sh
@@ -3,4 +3,4 @@ cp mysql.repo /etc/yum.repos.d/mysql.repo
 dnf install mysql-community-server -y
 systemctl enable mysqld
 systemctl restart mysqld
-mysql_secure_installtion --set-root-pass Roboshop@1
+mysql_secure_installtion --set-root-pass RoboShop@1
