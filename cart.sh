6ba236ec08f0f34f5498f845fc94d7e2aba99368
ravi <alluravikishore@gmail.com>
2025-03-28 14:52:34 +0530

    Merge remote-tracking branch 'origin/main'bashadengubasha

---
+++ cart.sh
@@ -0,0 +1,12 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
+dnf install nodejs -y
+useradd roboshop
+mkdir /app
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
+cd /app
+npm install
+cp /root/devops-practice/cart.service /etc/systemd/system/cart.service
+systemctl daemon-reload
+systemctl enable cart
+systemctl restart cart
